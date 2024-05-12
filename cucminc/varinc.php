9bcecfa816983d82cda63cdc4c52dd5595b1a1f2
comdif <142540893+comdif@users.noreply.github.com>
2024-05-12 11:20:10 +0200

    Create varinc.php

---
+++ cucminc/varinc.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = "127.0.0.1";
+$dbport = "3306";
+$dbname = "calldata";
+$dbuser = "your-mysql-login-here";
+$dbpwd = "your-mysql-password-here";
+//AXL VAR
+$axlhost="IP-of-the-CUCM";
+$axlusername="The_axl_username_you_define_on_CUCM";
+$axlpassword="The_axl_password_you_define_on_CUCM";
+?>
