87aad75ea91c971de077d3ba0bfcdbc4216989fe
comdif <142540893+comdif@users.noreply.github.com>
2024-05-12 11:31:11 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,54 @@
+<?php
+echo'<head>
+<style>
+table.blueTable { border: 5px double #1C6EA4;  background-color: #EEEEEE; width: 80%; height: px;
+text-align: left; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; padding: 2px 4px 2px 4px; }
+table.blueTable th { border: 1px solid #AAAAAA; padding: 6px 8px; }
+table.blueTable td { border: 1px solid #AAAAAA; padding: 6px 8px; }
+table.blueTable tbody td { font-size: 11px; font-weight: bold; }
+table.blueTable tr:nth-child(even) { background: #D0E4F5; }
+table.blueTable thead { background: #1C6EA4; background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%); background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%); border-bottom: 2px solid #444444; }
+table.blueTable thead th { font-size: 15px; font-weight: bold; olor: #FFFFFF; order-left: 2px solid #D0E4F5; }
+table.blueTable thead th:first-child { border-left: none; }
+table.blueTable tfoot { font-size: 14px; font-weight: bold; color: #FFFFFF; background: #D0E4F5; background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%); background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%); border-top: 2px solid #444444; }
+table.blueTable tfoot td { font-size: 14px; }
+table.blueTable tfoot .links { ext-align: right; }
+table.blueTable tfoot .links a { display: inline-block; background: #1C6EA4; }
+select { appearance: none; background-color: #E8E8E8; border: 1px solid; border-radius: 8px; padding: 2 2 2 0; margin: 0;  }
+</style>
+</head>';
+
+echo'<center><table class="blueTable">
+<thead>
+	<tr>
+		<th>
+		<a href="index.php">Cdrs</a>
+		</th>
+		<th>
+		<a href="impusers.php">Import/Update Users</a>
+		</th>
+		<th>
+ 
+		</th>
+		<th>
+
+		</th>
+		<th>
+
+		</th>
+		<th>
+
+		</th>
+		<th>
+ 
+		</th>
+		<th>
+
+		</th>
+
+	</tr>
+</thead>
+</table></center>';
+?>
