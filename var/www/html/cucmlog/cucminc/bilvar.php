e03a8b28d1f0f26409d4ee26230369a0bec3e9bd
Christian <142540893+comdif@users.noreply.github.com>
2024-06-15 12:39:33 +0200

    Create bilvar.php

---
+++ var/www/html/cucmlog/cucminc/bilvar.php
@@ -0,0 +1,14 @@
+<?php
+$ctrycode = "33";
+$lclprefix = "0";
+$trunk1 = "";
+$trunk2 = "";
+$trunk3 = "";
+$trunk4 = "";
+$trunk5 = "";
+$cucmprefix1 = "";
+$cucmprefix2 = "";
+$cucmprefix3 = "";
+$cucmprefix4 = "";
+$cucmprefix5 = "";
+?>
