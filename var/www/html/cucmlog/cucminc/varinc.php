e711718754c77967a5ee313d744f6713c76913b9
Christian <142540893+comdif@users.noreply.github.com>
2024-06-15 12:38:41 +0200

    Update varinc.php

--- var/www/html/cucmlog/cucminc/varinc.php
+++ var/www/html/cucmlog/cucminc/varinc.php
@@ -1,16 +1,10 @@
 <?php
 $dbhost = "127.0.0.1";
 $dbport = "3306";
+$dbuser = "cdrs_mega_big_boss";
+$dbpwd = "baila_bamba";
 $dbname = "calldata";
-// change for your custom informations
-$dbuser = "your-mysql-login-here";
-$dbpwd = "your-mysql-password-here";
-//AXL VAR
-$axlhost="IP-of-the-CUCM";
-$axlusername="The_axl_username_you_define_on_CUCM";
-$axlpassword="The_axl_password_you_define_on_CUCM";
-//BASH SCRIPT VAR
 $bashtble = "calldetails";
-//days to purge files but not database
-$bashpurgetime = "+10";
+include 'ciscovar.php';
+include 'bilvar.php';
 ?>
