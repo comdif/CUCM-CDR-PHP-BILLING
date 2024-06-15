0b815a1da0a2a7fc875a8553726f8305671b9580
Christian <142540893+comdif@users.noreply.github.com>
2024-06-15 12:05:43 +0200

    Update batch.sh

--- var/www/html/cucmlog/CDRS/batch.sh
+++ var/www/html/cucmlog/CDRS/batch.sh
@@ -1,53 +1,65 @@
 #!/bin/bash
 #V.01 updated: add a test to exit if there is no result & purge old archives
 #V.02 updated: no more var to define here all is in /var/www/html/cucmlog/cucminc/varinc.php
-########################################
+#V.03 updated: New dir CDRBACKUP to store original raw CDRS from CUCM
+################ christian@comdif.com ########################
+## RETENTION TIME FOR OLD RAW CDRS FILES (day)
+purgetime="+2"
 
 logpath=${PWD}
 archivepath="$logpath/ARCHIVE"
+backpath="$logpath/CDRBACKUP"
 current=$(dirname "$(pwd)")
 dbuser=$( cat $current/cucminc/varinc.php|grep dbuser|awk -F "\"" '{print $2}')
 dbpass=$( cat $current/cucminc/varinc.php|grep dbpwd|awk -F "\"" '{print $2}')
 mydb=$( cat $current/cucminc/varinc.php|grep dbname|awk -F "\"" '{print $2}')
 mytbl=$( cat $current/cucminc/varinc.php|grep bashtble|awk -F "\"" '{print $2}')
-purgetime=$( cat $current/cucminc/varinc.php|grep bashpurgetime|awk -F "\"" '{print $2}')
 
+	### PURGE OLD FILES
+echo "Purge old files from $backpath/ older than $purgetime days"
+find $backpath/ -type f -ctime $purgetime -delete
+
+	### GO to logpath and check if there is some CDRS received from the CUCM
 cd $logpath
-
 files=`ls $logpath|grep cdr`
 [[ -z "$files" ]] && { echo "nothing to do" ; exit 1; }
+
+	### OK there are CDRS we start by deleting two uninteresting lines and creating the ARCHIVE/cdr_file file with them
 for fse in $files
 	do
+	cp $logpath/$fse $backpath/
 	sed 1,2d "$fse" >> ARCHIVE/cdr_file
 	done
+
+	### Format the cdr_files and move it to a new file named in ARCHIVE/ named  cdr_clean the format must be like 'x','y','z' no comma on line start and line end
 tr -d '"' < $archivepath/cdr_file >> $archivepath/cdr_clean
 sed -i s/","/"','"/g $archivepath/cdr_clean
-#### Add quote at the start & end of lines
 sed -i s/^/"'"/g $archivepath/cdr_clean
 sed -i s/$/"'"/g $archivepath/cdr_clean
-##########################################
-################# Clean dir
+
+	### Now we can delete the previous ARCHIVE/cdr_file
 rm -f $archivepath/cdr_file
-###########################
+
+	## Go to the ARCHIVE path and populate the Database using the cdr_clean file
 cd $archivepath
-########################################## build the insert colum value
+
+	## build the insert colum value
 /usr/bin/mysql -u $dbuser -p$dbpass -e "select COLUMN_NAME from information_schema.columns where table_schema = '$mydb' and table_name = '$mytbl';" >> colum
 sed 1d colum >> clean_colum
 rm colum
 cat clean_colum | tr -s '\n' ','| awk '{ print substr( $0, 1, length($0)-1 ) }' >> colum
 rm clean_colum
-########################################################################
-#### put it in a var
+
+	## put it in a var
 tbcol=`cat colum`
-####################
-#### do the job
+
+	## Populate the Database
 while IFS= read -r line; do
 /usr/bin/mysql -u $dbuser -p$dbpass -e "INSERT INTO $mydb.$mytbl ($tbcol) VALUES ($line);"
-#echo "$line"
 done < cdr_clean
-rm -f $logpath/cdr*
-tar -czf cdr_clean-$(date +%F-%s).tgz cdr_clean
+## Clean all
 rm -f cdr_clean
 rm -f colum
-# PURGE OLD FILES
-find $archivepath/ -type f -ctime $purgetime -delete
+cd ..
+rm -f cdr*
+rm -f cmr*
