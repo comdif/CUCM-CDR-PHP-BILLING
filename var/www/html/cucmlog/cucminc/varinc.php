b0eb8a3da83b3e4d23505973f08e651d857324b1
Christian <142540893+comdif@users.noreply.github.com>
2024-06-15 12:41:45 +0200

    Update varinc.php

--- var/www/html/cucmlog/cucminc/varinc.php
+++ var/www/html/cucmlog/cucminc/varinc.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost = "127.0.0.1";
 $dbport = "3306";
-$dbuser = "cdrs_mega_big_boss";
-$dbpwd = "baila_bamba";
+$dbuser = "cdrsuser";
+$dbpwd = "pwd";
 $dbname = "calldata";
 $bashtble = "calldetails";
 include 'ciscovar.php';
